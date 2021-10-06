19ac25a23a6f7a4e727a1bc218cbbe1776f7e263
Alexander Vasyliev <gogleuser67967@gmail.com>
2021-10-06 16:08:42 +0300

    Removed unnecessary auto-generated methods and fields

--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -5,11 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
-export class OverviewComponent implements OnInit {
-
+export class OverviewComponent  {
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
